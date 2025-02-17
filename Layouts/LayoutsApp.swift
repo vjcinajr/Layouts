d565cc2d8fd451aec84e6b422f8b412e8b4338ff
Vincent Cina <vcina@me.com>
2025-02-17 14:44:13 -0500

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by VINCENT CINA on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
