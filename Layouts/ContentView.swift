e42a94cecf441701dee784c0eef67d6584f3ccb7
Vincent Cina <vcina@me.com>
2025-02-17 16:07:10 -0500

    Layouts part 2 - colors and spacing

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -12,7 +12,20 @@ struct ContentView: View {
     @State private var messageString = "'When the Genius Bar Needs Help, They Call You!'"
     
     var body: some View {
+        //        ZStack {
+        //            Color.yellow
+        //                .ignoresSafeArea()
         VStack {
+            
+//            Text("You Have Skills!")
+//                .font(.largeTitle)
+//                .fontWeight(.black)
+//                .foregroundStyle(.goldBC)
+//                .padding()
+//                .background(.maroonBC)
+//                .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            Spacer()
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
@@ -21,24 +34,33 @@ struct ContentView: View {
                 .foregroundColor(.red)
                 .frame(height: 150)
                 .frame(maxWidth: .infinity)
-                .border(.orange, width: 2)
+            //                .border(.orange, width: 2)
                 .padding()
+            
+            Spacer()
+            
             HStack {
                 Button("Awasome") {
                     messageString = "You Are Awasome!"
                 }
+                Spacer()
                 Button("Great") {
                     messageString = "You Are Great!"
                 }
             }
             .buttonStyle(.borderedProminent)
+            //            .border(.purple, width: 5)
+            .padding()
             
-    
             
-
+            
+            
         }
-        .padding()
+//        .background(.yellow.opacity(0.5))
+//        .background(.yellow.gradient)
+//        .padding()
     }
+    //    }
 }
 
 #Preview {
